6fc01e160f0cb72a92ad9a62e2986b43f38a34ef
Aaron Eduardo Cambi Chafla <75390416+Aaron-Cambi@users.noreply.github.com>
2021-01-21 17:35:42 -0500

    Touristic Places Of Quito

--- TouristicPlacesOfQuito/src/touristicplacesofquito/TouristicPlacesOfQuito.java
+++ TouristicPlacesOfQuito/src/touristicplacesofquito/TouristicPlacesOfQuito.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyect.go.navegation;
+package touristicplacesofquito;
 
 import java.util.Scanner;
 
@@ -11,22 +11,20 @@ import java.util.Scanner;
  *
  * @author Eduardo Cambi
  */
-public class ProyectGoNavegation {
+public class TouristicPlacesOfQuito {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        // TODO code application logic here
         Scanner input = new Scanner(System.in);
         String name;
         System.out.println("Enter name");
         name = input.nextLine();
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n" + 
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n" + 
             "| Touristic Places Of Quito |" + "\n" + 
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
-
+    
 }
-d

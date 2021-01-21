25d4b3d577f2b272ef670aa295e84ef4459481c5
Aaron Eduardo Cambi Chafla <75390416+Aaron-Cambi@users.noreply.github.com>
2021-01-21 17:20:02 -0500

    Project Touristic Places Of Quito

---
+++ Tourist places of Quito/src/proyect/go/navegation/ProyectGoNavegation.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyect.go.navegation;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Eduardo Cambi
+ */
+public class ProyectGoNavegation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String name;
+        System.out.println("Enter name");
+        name = input.nextLine();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n" + 
+            "| Touristic Places Of Quito |" + "\n" + 
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    }
+
+}
+d

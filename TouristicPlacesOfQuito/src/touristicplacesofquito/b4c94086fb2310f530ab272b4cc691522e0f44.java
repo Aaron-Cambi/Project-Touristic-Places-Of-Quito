85cfa55332b46466cfc35310d097ba2577c3e7cb
Alex228565 <alexvc228565@gmail.com>
2021-03-03 12:30:09 -0500

    proyect

---
+++ TouristicPlacesOfQuito/src/touristicplacesofquito/b4c94086fb2310f530ab272b4cc691522e0f44.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package touristicplacesofquito;
+
+/**
+ *
+ * @author Alex
+ */
+class b4c94086fb2310f530ab272b4cc691522e0f44 {
+    
+}
